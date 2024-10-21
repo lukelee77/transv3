fbff297cad8dbcbfdc73b81ef132c191001139e6
lukelee77 <lsess77@gmail.com>
2024-10-21 16:19:48 +0900

    Update AI_trans_test_v3.py

--- AI_trans_test_v3.py
+++ AI_trans_test_v3.py
@@ -13,7 +13,7 @@ def askGpt(prompt,apikey):
     client = openai.OpenAI(api_key = apikey)
 
     response = client.chat.completions.create(
-    model="gpt-4o",
+    model="gpt-4o-mini",
     messages=[{"role": "user", "content": prompt}])
     gptResponse = response.choices[0].message.content
     return gptResponse    
@@ -24,7 +24,7 @@ def main():
     st.set_page_config(page_title="Translator GPT", page_icon="📖", layout="wide")
     st.title('''Luke's AI 번역 프로그램 테스트_Ver3😏''')
     st.subheader('※ 배포금지. 개인용 유료 API key 사용 (해외시장 전용)')    
-    st.text('''Instruction - 아래 번역칸에 "No.국가명 --- 제품불량" 형태로 입력(최대 500줄이하)''')    
+    st.text('''Instruction - 아래 번역칸에 "No.국가명 --- 제품불량" 형태로 입력(최대 300줄이하)''')    
     st.text('''출력 형태는 "No. 국가명 --- 제품 불량 --- 한국어 번역결과 --- 요약 증상" ''')    
     st.markdown('---')
   
@@ -37,7 +37,7 @@ def main():
     - 어떤 국가의 언어인지와 제품 불량 데이터를 구분하기 위해 두 가지 데이터 사이에 --- 이라는 구분자 기호를 사용할 거야.
     - 그러면 국가명을 참조해서 제품 불량을 한국어로 번역해줘.
     - 화면에 보여줄 결과 값은 국가명과 제품 불량을 한국어로 번역한 결과를 반드시 한 줄씩 보여줘.
-    - 결과는 반드시 생략하지 말고 최대 500줄을 빠짐없이 모두 한 줄씩 순서대로 보여주고, 결과물 앞에 번호를 붙여줘.
+    - 결과는 반드시 생략하지 말고 최대 300줄을 빠짐없이 모두 한 줄씩 순서대로 보여주고, 결과물 앞에 번호를 붙여줘.
     - 한국어로 번역한 결과를 요약해서 간략한 요약 증상도 보여줘.
 
     - 출력 양식은 아래를 참조해줘.
